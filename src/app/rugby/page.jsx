bc1e36075b05a0a80fe2f541993e54fe5d6a4565
jwebchajari <jwebchajari@gmail.com>
2026-08-21 17:37:58 -0300

    rugby

---
+++ src/app/rugby/page.jsx
@@ -0,0 +1,9 @@
+// src/app/rugby/page.jsx
+export default function RugbyPage() {
+    return (
+        <div>
+            <h1>Página de Rugby</h1>
+            {/* Resto del contenido */}
+        </div>
+    );
+}
